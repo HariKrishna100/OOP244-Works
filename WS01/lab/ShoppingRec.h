e1f179f6b213a7ddab5d32641c5db95f5d7c364e
Harikrishna Paresh Patel <Hpatel296@myseneca.ca>
2022-09-16 11:38:41 -0400

    Added names comments

--- WS01/lab/ShoppingRec.h
+++ WS01/lab/ShoppingRec.h
@@ -1,3 +1,18 @@
+/*
+*****************************************************************************
+						  Lab - #1 (P1)
+Full Name  : Harikrishna Paresh Patel
+Student ID#: 150739217
+Email      : Hpatel296@myseneca.ca
+Section    : NAA
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
+
 #ifndef SDDS_SHOPPINGREC_H
 #define SDDS_SHOPPINGREC_H
 namespace sdds {
