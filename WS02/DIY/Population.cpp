b0bc41219aec4e717041a64c6dd6e6cd8c28e28c
HariKrishna100 <hpatel296@myseneca.ca>
2022-09-28 23:26:36 -0400

    fixed library string.h strcpy

--- WS02/DIY/Population.cpp
+++ WS02/DIY/Population.cpp
@@ -1,3 +1,16 @@
+/*
+*****************************************************************************
+                          Lab - #1 (P1)
+Full Name  : Harikrishna Paresh Patel
+Student ID#: 150739217
+Email      : Hpatel296@myseneca.ca
+Section    : NAA
+Date       : 16/09/2022
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my professor
+provided to complete my workshops and assignments.
+*****************************************************************************
+*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string.h>
--- WS02/DIY/Population.h
+++ WS02/DIY/Population.h
@@ -1,3 +1,16 @@
+/*
+*****************************************************************************
+                          Lab - #1 (P1)
+Full Name  : Harikrishna Paresh Patel
+Student ID#: 150739217
+Email      : Hpatel296@myseneca.ca
+Section    : NAA
+Date       : 16/09/2022
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my professor
+provided to complete my workshops and assignments.
+*****************************************************************************
+*/
 #ifndef SDDS_POPULATION_H_
 #define SDDS_POPULATION_H_
 namespace sdds {
