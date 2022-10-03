0b0fa91e4e0eed03d7f5757309dc474974cd1a71
HariKrishna100 <hpatel296@myseneca.ca>
2022-10-02 21:29:13 -0400

    Fixed cout.fill read bugs

--- WS03/lab/CC.cpp
+++ WS03/lab/CC.cpp
@@ -1,3 +1,16 @@
+/*
+*****************************************************************************
+						  Lab - #3
+Full Name  : Harikrishna Paresh Patel
+Student ID#: 150739217
+Email      : Hpatel296@myseneca.ca
+Section    : NAA
+Date       : 30/09/2022
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my professor
+provided to complete my workshops and assignments.
+*****************************************************************************
+*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "CC.h"
@@ -34,23 +47,28 @@ namespace sdds {
 		cout.width(4);
 		cout.fill('0');
 		cout.setf(ios::right);
-		cout << m_cardNumber / 1000000000000 << endl;
+		cout << m_cardNumber / 1000000000000 << " ";
+		cout.fill(' ');
+		cout.unsetf(ios::right);
 
 		cout.width(4);
 		cout.fill('0');
 		cout.setf(ios::right);
-		cout << (m_cardNumber % 1000000000000) / 100000000 << endl;
+		cout << (m_cardNumber % 1000000000000) / 100000000 << " ";
+		cout.fill(' ');
+		cout.unsetf(ios::right);
 
 		cout.width(4);
 		cout.fill('0');
 		cout.setf(ios::right);
-		cout << ((m_cardNumber % 1000000000000) % 100000000) / 10000 << endl;
+		cout << ((m_cardNumber % 1000000000000) % 100000000) / 10000 << " ";
+		cout.fill(' ');
+		cout.unsetf(ios::right);
 
 		cout.width(4);
 		cout.fill('0');
 		cout.setf(ios::right);
-		cout << ((((m_cardNumber % 1000000000000) % 1000000000) % 1000000) % 10000) << endl;
-
+		cout << ((((m_cardNumber % 1000000000000) % 1000000000) % 1000000) % 10000);
 		cout.fill(' ');
 		cout.unsetf(ios::right);
 	}
@@ -96,85 +114,78 @@ namespace sdds {
 		char name[71];
 		short cvv = 0, expiryMonth = 0, expiryYear = 0;
 		long long cardNo = 0;
-		bool success = false;
 
 		cleanUp();
 
-		if (cin.fail())
-		{
+		if (cin.fail()) {
 			cin.clear();
 			cin.ignore(1000, '\n');
 		}
-		else
-		{
-			if (cin) cout << "Card holder name: ";
-			cin.getline(name, 71);
-			if (cin) cout << "Credit card number: ";
-			cin >> cardNo;
-			if (cin) cout << "Card Verification Value (CVV): ";
-			cin >> cvv;
-			if (cin) cout << "Expiry month and year (MM/YY): ";
-			cin >> expiryMonth;
-			cin.ignore(1, '/');
-			cin >> expiryYear;
-		}
-		if (!cin.fail())
-		{
+		if (cin) cout << "Card holder name: ";
+		cin.getline(name, 71);
+		if (cin) cout << "Credit card number: ";
+		cin >> cardNo;
+		if (cin) cout << "Card Verification Value (CVV): ";
+		cin >> cvv;
+		if (cin) cout << "Expiry month and year (MM/YY): ";
+		cin >> expiryMonth;
+		cin.ignore(1, '/');
+		cin >> expiryYear;
+		if (!cin.fail()) {
 			set(name, cardNo, cvv, expiryMonth, expiryYear);
 		}
 		cin.ignore(1000, '\n');
 		return !isEmpty();
 	}
 
-	void CC::display(int row = 0) const {
-		bool empty = isEmpty();
+	void CC::display(int row) const {
+		bool null = isEmpty();
 		char name[1000];
-		if (!empty)
-		{
-			if (row > 0)
-			{
-				cout << "| " << right;
-				cout.setf(3);
-				cout << row << " ";
+		if (!null) {
+			if (row > 0) {
+				cout << "| ";
+				cout.width(3);
+				cout.setf(ios::right);
+				cout << row << " |";
+				cout.unsetf(ios::right);
 
-				if (strlen(m_Cardholdername) > 30)
-				{
+				if (strlen(m_Cardholdername) > 30) {
 					strcpy(name, m_Cardholdername);
-					for (int i = 30; i < strlen(m_Cardholdername); i++)
-					{
+					for (int i = 30; i < strlen(m_Cardholdername); i++) {
 						name[i] = '\0';
 					}
 					strcpy(m_Cardholdername, name);
 				}
-				cout << "| " << left;
-				cout.setf(30);
-				cout << m_Cardholdername << " ";
-				cout.setf(ios::left);
-				cout << "| ";
-
-				prnNumber();
 
 				cout << " ";
+				cout.width(30);
+				cout.setf(ios::left);
+				cout << m_Cardholdername << " ";
+				cout.unsetf(ios::left);
 
-				cout << "| " << m_CVV << " ";
-				cout << "| " << right;
-				cout.setf(2);
+				cout << "| ";
+				prnNumber();  
+				cout << " ";
+
+				cout << "| " << m_CVV << " |";
+
+				cout << " ";
+				cout.width(2);
+				cout.fill('| ');
+				cout.setf(ios::right);
 				cout << m_expMon << "/" << M_expYear << " |" << endl;
-
+				cout.fill(' ');
+				cout.unsetf(ios::right);
 			}
-			else if (row <= 0)
-			{
+			else if (row <= 0) {
 				cout << "Name: " << m_Cardholdername << endl;
 				cout << "Creditcard No: ";
-
 				prnNumber();
-
 				cout << endl << "Card Verification Value: " << m_CVV << endl;
 				cout << "Expiry Date: " << m_expMon << "/" << M_expYear << endl;
 			}
 		}
-		else
-		{
+		else {
 			cout << "Invalid Credit Card Record" << endl;
 		}
 	}
--- WS03/lab/CC.h
+++ WS03/lab/CC.h
@@ -1,3 +1,16 @@
+/*
+*****************************************************************************
+						  Lab - #3
+Full Name  : Harikrishna Paresh Patel
+Student ID#: 150739217
+Email      : Hpatel296@myseneca.ca
+Section    : NAA
+Date       : 30/09/2022
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my professor
+provided to complete my workshops and assignments.
+*****************************************************************************
+*/
 #ifndef SDDS_CC_H_
 #define SDDS_CC_H_
 namespace sdds {
