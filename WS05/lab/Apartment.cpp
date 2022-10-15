e15eac44ebb69f82ea63c1c750a1f13f88d71443
HariKrishna100 <hpatel296@myseneca.ca>
2022-10-14 23:34:08 -0400

    fixed operator const

--- WS05/lab/Apartment.cpp
+++ WS05/lab/Apartment.cpp
@@ -1,3 +1,16 @@
+/*
+*****************************************************************************
+                          Lab - #5
+Full Name  : Harikrishna Paresh Patel
+Student ID#: 150739217
+Email      : Hpatel296@myseneca.ca
+Section    : NAA
+Date       : 14/10/2022
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my professor
+provided to complete my workshops and assignments.
+*****************************************************************************
+*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "Apartment.h"
@@ -36,17 +49,109 @@ namespace sdds
       }
       return cout;
    }
-   Apartment::operator bool() const
-   {
+   
+   // check if the object is not empty
+   Apartment::operator bool() const {
+       return ((m_number >= 1000 && m_number <= 9999) && (m_balance >= 0.0));
    }
-   Apartment::operator int() const
-   {
+    
+   // return appartmentNo
+   Apartment::operator int() const {
+       return m_number;
    }
-   Apartment::operator double() const
-   {
+
+   // return rentBalance
+   Apartment::operator double() const {
+       return m_balance;
    }
-   bool Apartment::operator~()
+
+   // check if apartmentbalance = 0
+   bool Apartment::operator~() const {
+       return m_balance == 0;
+   }
+
+   // assign apartment to int
+   Apartment& Apartment::operator=(int appNum)
    {
-       return false;
+       if (appNum >= 1000 && appNum <= 9999) {
+           m_number = appNum;
+       }
+       else {
+           m_number = -1;
+           m_balance = 0;
+       }
+       return *this;
+   }
+
+   // swap values to another AP Object 
+   Apartment& Apartment::operator=(Apartment& RO) {
+       int num = 0;
+       double bal = 0.0;
+       if (this != &RO) {
+           num = m_number;
+           bal = m_balance;
+           m_number = RO.m_number;
+           m_balance = RO.m_balance;
+           RO.m_number = num;
+           RO.m_balance = bal;
+       }
+       return *this;
+   }
+
+   // add double value to rent
+   Apartment& Apartment::operator+=(double value) {
+       if (*this && (m_balance > 0.0) && (value > 0.0)) {
+           m_balance += value;
+       }
+       return *this;
+   }
+
+   // reduce rent by double value
+   Apartment& Apartment::operator-=(double value) {
+       if (*this && (m_balance > 0.0) && (value > 0.0) && (value < m_balance)) {
+           m_balance -= value;
+       }
+       return *this;
+   }
+
+   // Pour balance from Right to Left
+   Apartment& Apartment::operator<<(Apartment& RO) {
+       double bal = 0.0;
+       if (*this && RO && (this != &RO)) {
+           bal = RO.m_balance;
+           RO.m_balance = 0.0;
+           m_balance += bal;
+       }
+       return *this;
+   }
+
+   // Pour balance from Left to Right
+   Apartment& Apartment::operator>>(Apartment& RO) {
+       double bal = 0.0;
+       if (*this && RO && (this != &RO)) {
+           bal = m_balance;
+           m_balance = 0.0;
+           RO.m_balance += bal;
+       }
+       return *this;
+   }
+
+   // sum balance of apartments
+   double operator+(const Apartment& LO, const Apartment& RO) {
+       double bal = 0.0, loValue = LO, roValue = RO;
+       if (LO && RO) {
+           bal = loValue + roValue;
+       }
+       return bal;
+   }
+
+   // sum left + right
+   double operator+=(double& LO, const Apartment& RO) {
+       double bal = 0.0, roValue = 0.0;
+       if (RO) {
+           roValue = RO;
+           bal = LO += roValue;
+       }
+       return bal;
    }
 }
--- WS05/lab/Apartment.h
+++ WS05/lab/Apartment.h
@@ -1,3 +1,16 @@
+/*
+*****************************************************************************
+                          Lab - #5
+Full Name  : Harikrishna Paresh Patel
+Student ID#: 150739217
+Email      : Hpatel296@myseneca.ca
+Section    : NAA
+Date       : 14/10/2022
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my professor
+provided to complete my workshops and assignments.
+*****************************************************************************
+*/
 #pragma once
 #ifndef SDDS_APARTMENT_H_
 #define SDDS_APARTMENT_H_
@@ -16,9 +29,17 @@ namespace sdds
       operator bool()const;
       operator int()const;
       operator double()const;
-      bool operator ~();
-
+      bool operator ~()const;
+      Apartment& operator=(int appNum);
+      Apartment& operator=(Apartment& RO);
+      Apartment& operator+=(double RO);
+      Apartment& operator-=(double RO);
+      Apartment& operator<<(Apartment& RO);
+      Apartment& operator>>(Apartment& RO);
    };
+   // binary helper Operators
+   double operator+(const Apartment& LO, const Apartment& RO);
+   double operator+=(double& LO, const Apartment& RO);
 }
 
 #endif // SDDS_APARTMENT_H_
