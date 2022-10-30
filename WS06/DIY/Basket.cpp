614f10a81ec71c626763b69334a02aa80e6f8589
HariKrishna100 <hpatel296@myseneca.ca>
2022-10-29 22:48:25 -0400

    rule of three

--- WS06/DIY/Basket.cpp
+++ WS06/DIY/Basket.cpp
@@ -1,5 +1,119 @@
+/*
+*****************************************************************************
+                          Diy - #6
+Full Name  : Harikrishna Paresh Patel
+Student ID#: 150739217
+Email      : Hpatel296@myseneca.ca
+Section    : NAA
+Date       : 29/10/2022
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my professor
+provided to complete my workshops and assignments.
+*****************************************************************************
+*/
 #include <iostream>
+#include <iomanip>
+#include "Basket.h"
 using namespace std;
 namespace sdds {
+    // defualt constructor
+    Basket::Basket() {
+        setEmpty();
+    }
 
+    // three argument constructor
+    Basket::Basket(Fruit* fruits, int arraySize, double basketPrice) {
+        if (basketPrice > 0 && arraySize > 0 && fruits != nullptr) {
+            m_Fruit = new Fruit[arraySize];
+            for (int i = 0; i < arraySize; i++) {
+                m_Fruit[i] = fruits[i];
+            }
+            m_Size = arraySize;
+            m_Price = basketPrice;
+        }
+    }
+
+    // Copy constructor
+    Basket::Basket(const Basket& toCopy) {
+        setEmpty();
+        operator=(toCopy);
+    }
+
+    // copy assignment operator
+    Basket& Basket::operator=(const Basket& toCopy) {
+        delete[] m_Fruit;
+        setEmpty();
+
+        m_Fruit = new Fruit[toCopy.m_Size];
+        m_Size = toCopy.m_Size;
+        m_Price = toCopy.m_Price;
+
+        for (int i = 0; i < toCopy.m_Size; i++) {
+            m_Fruit[i] = toCopy.m_Fruit[i];
+        }
+        return *this;
+    }
+
+    // deallocate memory
+    Basket::~Basket() {
+        delete[] m_Fruit;
+    }
+
+    // set Basket to safe empty
+    void Basket::setEmpty() {
+        m_Fruit = nullptr;
+        m_Size = 0;
+        m_Price = 0;
+    }
+
+    // update price by argument received 
+    void Basket::setPrice(double price) {
+        m_Price = price;
+    }
+
+    // check if basket contanins fruits
+    Basket::operator bool() const {
+        return (m_Size != 0);
+    }
+
+    // add object Fruit by value to m_fruits
+    Basket& Basket::operator+=(Fruit fruits) {
+        Fruit* temp = new Fruit[m_Size + 1];
+
+        for (int i = 0; i < m_Size; i++) {
+            temp[i] = m_Fruit[i];
+        }
+        m_Size++;
+
+        temp[m_Size - 1] = fruits;
+        delete[] m_Fruit;
+
+        m_Fruit = temp;
+
+        return *this;
+    }
+
+    // display 
+    std::ostream& Basket::display(std::ostream& ostr) const {
+        ostr << setprecision(2) << fixed;
+
+        if (m_Size > 0) {
+            ostr << "Basket Content:" << endl;
+            for (int i = 0; i < m_Size; i++) {
+                ostr.width(10);
+                ostr.setf(ios::right);
+                ostr << m_Fruit[i].m_name << ": " << m_Fruit[i].m_qty << "kg" << endl;
+            }
+            ostr << "Price: " << m_Price << endl;
+        }
+        else {
+            ostr << "The basket is empty!" << endl;
+        }
+        return ostr;
+    }
+
+    // friend helper 
+    std::ostream& operator<<(std::ostream& os, const Basket& B) {
+        return B.display(os);
+    }
 }
--- WS06/DIY/Basket.h
+++ WS06/DIY/Basket.h
@@ -1,7 +1,44 @@
+/*
+*****************************************************************************
+                          Diy - #6
+Full Name  : Harikrishna Paresh Patel
+Student ID#: 150739217
+Email      : Hpatel296@myseneca.ca
+Section    : NAA
+Date       : 29/10/2022
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my professor
+provided to complete my workshops and assignments.
+*****************************************************************************
+*/
 #ifndef SDDS_BASKET_H_
 #define SDDS_BASKET_H_
+#include <iostream>
 namespace sdds {
+    struct Fruit
+    {
+        char m_name[30 + 1]; // the name of the fruit
+        double m_qty;        // quantity in kilograms
+    };
 
+    class Basket {
+    private:
+        Fruit* m_Fruit;
+        int m_Size;
+        double m_Price;
+    public:
+        Basket();
+        Basket(Fruit* fruits, int arraySize, double basketPrice);
+        Basket(const Basket& toCopy);
+        Basket& operator=(const Basket& toCopy);
+        ~Basket();
+        void setEmpty();
+        void setPrice(double price);
+        operator bool()const;
+        Basket& operator+=(Fruit fruits);
+        std::ostream& display(std::ostream& ostr)const;
+    };
+    std::ostream& operator<<(std::ostream& os, const Basket& B);
 }
 #endif // !SDDS_BASKET_H_
 
